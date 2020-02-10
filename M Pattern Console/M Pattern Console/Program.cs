8be21940d798f5f0746b1755a254e056ee9e7228
djengo16 <53972425+djengo16@users.noreply.github.com>
2020-02-11 01:32:21 +0200

    update
    update

--- M Pattern Console/M Pattern Console/Program.cs
+++ M Pattern Console/M Pattern Console/Program.cs
@@ -8,7 +8,20 @@ namespace M_Pattern_Console
         {
             Console.Write("Please choose the letter width: ");
             int letterWidth = int.Parse(Console.ReadLine());
-
+            while (true) {
+                if (letterWidth < 2 && letterWidth < 10 - 000)
+                {
+                    Console.WriteLine("Number is too small or too large!\nPrealse Try again!");
+                    letterWidth = int.Parse(Console.ReadLine());
+                }
+                else if (letterWidth % 2 == 0)
+                {
+                    Console.WriteLine("Number should be odd!\nPlease Try again!");
+                    letterWidth = int.Parse(Console.ReadLine());
+                }
+                else break;
+                
+            }
             Field field = new Field(letterWidth);
             field.GenerateInitialField();
             field.Modify();
